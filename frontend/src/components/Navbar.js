cc449385cdb8efb15d7a3464d91dd2c485838584
Foad Botan <foadbotan@gmail.com>
2022-10-26 22:00:27 +1100

    Extra padding for navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="flex py-10 gap-5 items-center">
+    <header className="flex py-2 gap-5 items-center">
       <div>
         <span className="bg-red-400 p-2 rounded-full text-white mr-1">✈️</span>
         <span className="text-lg">Travel</span>
